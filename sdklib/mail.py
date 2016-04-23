9f126b83be24e2b9ba46a5fc42389c0947e15e12
Ivan Martin <ivanprjcts@gmail.com>
2016-04-24 00:36:56 +0200

    add imap client

--- sdklib/mail.py
+++ sdklib/mail.py
@@ -1,5 +1,46 @@
 import poplib
+import imaplib
+import exceptions
+import email
+import datetime
+
 from email import parser
+
+
+def add_email_domain_if_it_is_not_added(email_adress, domain):
+    if '@' not in email_adress:
+        return "%s@%s" % email_adress, domain
+    else:
+        return email_adress
+
+
+def from_email_message_to_json(msg):
+    json_obj_to_return = dict()
+    msg = email.message_from_string(msg)
+    decode = email.Header.decode_header(msg['Subject'])[0]
+    json_obj_to_return['subject'] = unicode(decode[0], encoding=decode[1])
+    json_obj_to_return['date'] = msg['Date']
+    # Now convert to local date-time
+    date_tuple = email.utils.parsedate_tz(json_obj_to_return['date'])
+    if date_tuple:
+        local_date = datetime.datetime.fromtimestamp(
+            email.utils.mktime_tz(date_tuple))
+        json_obj_to_return['localDate'] = local_date.strftime("%a, %d %b %Y %H:%M:%S")
+
+    if msg.is_multipart():
+        for part in msg.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get('Content-Disposition'))
+
+            # skip any text/plain (txt) attachments
+            if ctype == 'text/plain' and 'attachment' not in cdispo:
+                json_obj_to_return['payload'] = part.get_payload(decode=True)  # decode
+                break
+                # not multipart - i.e. plain text, no attachments, keeping fingers crossed
+    else:
+        json_obj_to_return['payload'] = msg.get_payload()
+
+    return json_obj_to_return
 
 
 class POPMailSdk(object):
@@ -27,3 +68,51 @@ class GmailPOPlSdk(POPMailSdk):
 
     def __init__(self, user, password):
         super(GmailPOPlSdk, self).__init__('pop.gmail.com', user, password)
+
+
+class IMAPMailSdk(object):
+
+    def __init__(self, host, user, password):
+        self.host = host
+        self.user = user
+        self.password = password
+
+    @staticmethod
+    def process_mailbox(M):
+        """
+        Do something with emails messages in the folder.
+        For the sake of this example, print some headers.
+        """
+        messages = []
+
+        rv, data = M.search(None, "ALL")
+        if rv != 'OK':
+            print "No messages found!"
+            return
+
+        for num in data[0].split():
+            rv, data = M.fetch(num, '(RFC822)')
+            if rv != 'OK':
+                print "ERROR getting message", num
+                return
+
+            msg = from_email_message_to_json(data[0][1])
+            messages.append(msg)
+        return messages
+
+    def read_all_mails(self):
+        mail = imaplib.IMAP4_SSL(self.host)
+        try:
+            mail.login(self.user, self.password)
+        except imaplib.IMAP4.error:
+            raise exceptions.BaseException("Login failed")
+
+        mail.select("inbox")  # connect to inbox.
+        data = self.process_mailbox(mail)
+        return data
+
+
+class GmailIMAPlSdk(IMAPMailSdk):
+
+    def __init__(self, user, password):
+        super(GmailIMAPlSdk, self).__init__('imap.gmail.com', user, password)
--- tests/test_mail.py
+++ tests/test_mail.py
@@ -1,6 +1,6 @@
 import unittest
 
-from sdklib.mail import GmailPOPlSdk
+from sdklib.mail import GmailPOPlSdk, GmailIMAPlSdk
 
 
 class TestGmailPOPSdk(unittest.TestCase):
@@ -19,3 +19,18 @@ class TestGmailPOPSdk(unittest.TestCase):
         for m in messages:
             print dir(m)
 
+
+class TestGmailIMAPSdk(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.api = GmailIMAPlSdk('sdklib.test', 'sdklib12345678')
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_read_all_mails(self):
+        messages = self.api.read_all_mails()
+        for m in messages:
+            print m
