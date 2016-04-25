9990b42dc564e144e30b2233eee31d7b14c83428
Ivan Martin <ivanprjcts@gmail.com>
2016-04-25 17:24:31 +0200

    more code

--- sdklib/sdklib.py
+++ sdklib/sdklib.py
@@ -8,15 +8,9 @@ import json
 import collections
 import urllib3
 
-try:
-    from urllib.parse import urlencode
-except:
-    from urllib import urlencode
-
-
+from .compat import urlencode
 from .util.urls import get_hostname_parameters_from_url, ensure_url_path_starts_with_slash
-from .util.file import guess_filename_stream
-from .util.parser import parse_params_as_tuple_list
+from .renderers import JSONRender, MultiPartRender
 
 
 class SdkResponse(object):
@@ -46,17 +40,10 @@ class SdkBase(object):
     CONNECTION_HEADER_NAME = "Connection"
     REFERRER_HEADER_NAME = "Referer"
 
-    CONTENT_TYPE_JSON = "application/json"
-    CONTENT_TYPE_URL_ENCODED = "application/x-www-form-urlencoded"
-    CONTENT_TYPE_OCTET = "application/octet-stream"
-    CONTENT_TYPE_PLAIN_TEXT = "text/plain; charset=utf-8"
-    CONTENT_TYPE_MULTIPART_FORM_DATA = "multipart/form-data"
-
-    BOUNDARY = "----------ThIs_Is_tHe_bouNdaRY_$"
-
-    def __init__(self):
-        self.host = "http://127.0.0.1:80/"
-        self.proxy = None
+    def __init__(self, host='http://127.0.0.1:80/', proxy=None, default_render=JSONRender()):
+        self.host = host
+        self.proxy = proxy
+        self.default_render = default_render
 
     @property
     def host(self):
@@ -91,33 +78,6 @@ class SdkBase(object):
         """
         self._proxy = value
 
-    @staticmethod
-    def encode_multipart_formdata(fields, files):
-        """
-        fields is a sequence of (name, value) elements for regular form fields.
-        files is a sequence of (name, value) elements for data to be uploaded as files
-        Return (content_type, body) ready for httplib.HTTP instance
-        """
-        CRLF = '\r\n'
-        L = []
-        for (key, value) in fields:
-            L.append('--' + SdkBase.BOUNDARY)
-            L.append('Content-Disposition: form-data; name=%s' % key)
-            L.append('Content-Type: %s' % SdkBase.CONTENT_TYPE_JSON)
-            L.append('')
-            L.append(str(value))
-        for (key, value) in files:
-            filename, stream = guess_filename_stream(value)
-            L.append('--' + SdkBase.BOUNDARY)
-            L.append('Content-Disposition: form-data; name=%s; filename="%s"' % (key, filename))
-            L.append('Content-Type: %s' % SdkBase.CONTENT_TYPE_OCTET)
-            L.append('')
-            L.append(stream)
-        L.append('--' + SdkBase.BOUNDARY + '--')
-        L.append('')
-        body = CRLF.join(L)
-        return body
-
     def default_headers(self):
         return dict()
 
@@ -137,48 +97,30 @@ class SdkBase(object):
             headers to add for the file.
         :return:
         """
+        host = kwargs.get('host', self.host)
+        proxy = kwargs.get('proxy', self.proxy)
+        render = kwargs.get('render', self.default_render)
+
         method = method.upper()
         assert method in ['GET', 'HEAD', 'DELETE', 'POST', 'PUT', 'PATCH', 'OPTIONS', 'TRACE', 'CONNECT']
 
         url_path = ensure_url_path_starts_with_slash(url_path)
 
-        url = "%s%s" % (self.host, url_path)
+        url = "%s%s" % (host, url_path)
         if query_params is not None:
             url += "?%s" % (urlencode(query_params))
 
         if files:
-            try:
-                files = parse_params_as_tuple_list(files)
-            except TypeError:
-                pass
-            try:
-                fields = parse_params_as_tuple_list(body_params)
-            except TypeError:
-                fields = body_params
-            body = self.encode_multipart_formdata(fields=fields, files=files)
-            headers[self.CONTENT_TYPE_HEADER_NAME] = '%s; boundary=%s' % (self.CONTENT_TYPE_MULTIPART_FORM_DATA,
-                                                                          self.BOUNDARY)
-        elif form_urlencoding:
-            if body_params is not None:
-                parameters = urlencode(body_params)
-            else:
-                parameters = ""
-            xHeaders[self.CONTENT_TYPE_HEADER_NAME] = self.CONTENT_TYPE_URL_ENCODED
-        elif isinstance(body_params, structures.Mapping):
-            try:
-                parameters = json.dumps(body_params)
-            except:
-                parameters = json.dumps(body_params, encoding='latin-1')
-            xHeaders[self.CONTENT_TYPE_HEADER_NAME] = self.CONTENT_TYPE_JSON
-        else:
-            parameters = body_params
-            xHeaders[self.CONTENT_TYPE_HEADER_NAME] = self.CONTENT_TYPE_PLAIN_TEXT
+            render = MultiPartRender()
+
+        body, content_type = render.encode_params(body_params, files=files)
 
         if headers is None:
             headers = self.default_headers()
+            headers[self.CONTENT_TYPE_HEADER_NAME] = content_type
 
         r = self.pool_manager.request(method, url,
-                                      fields=body_params,
+                                      fields=body,
                                       encode_multipart=True,
                                       headers=headers)
 
