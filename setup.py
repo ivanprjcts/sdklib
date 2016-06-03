e8b341455e027689ce10cf0dc04fe374b039a498
Ivan Martin <ivan.martin@11paths.com>
2016-06-03 08:27:03 +0200

    update license and python versions

--- setup.py
+++ setup.py
@@ -7,18 +7,22 @@ REQUIRES = ["urllib3 >= 1.10"]
 
 setup(
     name='sdklib',
-    version='1.0',
+    version='1.1',
     description='SDK helper library',
     author='Ivan Martin',
     author_email='ivanprjcts@gmail.com',
     url='https://github.com/ivanprjcts/sdklib',
     install_requires=REQUIRES,
+    license='BSD',
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     zip_safe=False
