fa3cdd529534e6cca363755dfad2488aba59e3dd
ivan.martin <ivan.martin@11paths.com>
2016-07-27 08:20:38 +0200

    add timeout function

--- sdklib/util/times.py
+++ sdklib/util/times.py
@@ -1,5 +1,15 @@
 import time
 import datetime
+from functools import wraps
+from multiprocessing import TimeoutError
+from multiprocessing.pool import ThreadPool
+import threading
+import weakref
+
+from sdklib.compat import thread
+
+
+thread_pool = None
 
 
 def get_current_utc(time_format="%Y-%m-%d %H:%M:%S"):
@@ -37,8 +47,27 @@ def datetime_to_milliseconds_timestamp(datetime_obj):
     return seconds_to_milliseconds_timestamp(seconds_timestamp)
 
 
-def get_current_utc():
-    """
-    @return a string representation of the current time in UTC to be used in a Date HTTP Header
-    """
-    return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_thread_pool():
+    global thread_pool
+    if thread_pool is None:
+        # fix for python <2.7.2
+        if not hasattr(threading.current_thread(), "_children"):
+            threading.current_thread()._children = weakref.WeakKeyDictionary()
+        thread_pool = ThreadPool(processes=1)
+    return thread_pool
+
+
+def timeout(milliseconds, silent=False):
+    def wrap_function(func):
+        @wraps(func)
+        def __wrapper(*args, **kwargs):
+            try:
+                async_result = get_thread_pool().apply_async(func, args=args, kwds=kwargs)
+                return async_result.get(float(milliseconds) / 1000)
+            except thread.error:
+                return func(*args, **kwargs)
+            except TimeoutError:
+                pass
+
+        return __wrapper
+    return wrap_function
