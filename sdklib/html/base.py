49015531237db1026b21968a44137175ddcde74e
Ivan Martin <ivanprjcts@gmail.com>
2016-12-13 19:39:23 +0100

    add find_elements_by_id and other methods to html object

--- sdklib/html/base.py
+++ sdklib/html/base.py
@@ -2,16 +2,50 @@ import html5lib
 
 
 class HTML(object):
+    """
+    HTMLObject class.
+    """
 
     def __init__(self, dom):
         self._parse(dom=dom)
+        self._remove_namespaces()
 
     def _parse(self, dom):
         self.tree = html5lib.parse(dom)
 
+    def _remove_namespaces(self):
+        for el in self.tree.iter():
+            if isinstance(el.tag, str) and '}' in el.tag:
+                el.tag = el.tag.split('}', 1)[1]  # strip all namespaces
+
     def find_element_by_id(self, id_):
-        pass
+        """
+        Finds an element by id.
+        :param id_: The id of the element to be found.
+        :return:
+        """
+        return self.find_element_by_xpath('.//*[@id="%s"]' % id_)
+
+    def find_elements_by_id(self, id_):
+        """
+        Finds multiple elements by id.
+        :param id_: The id of the elements to be found.
+        :return:
+        """
+        return self.find_elements_by_xpath('.//*[@id="%s"]' % id_)
 
     def find_element_by_xpath(self, xpath):
-        self.tree.findall(xpath)
+        """
+        Finds an element by xpath.
+        :param xpath: The xpath locator of the element to find.
+        :return:
+        """
+        return self.tree.find(xpath)
+
+    def find_elements_by_xpath(self, xpath):
+        """
+        Finds multiple elements by xpath.
+        :param xpath: The xpath locator of the elements to be found.
+        :return:
+        """
         return self.tree.findall(xpath)
--- tests/test_html.py
+++ tests/test_html.py
@@ -11,12 +11,16 @@ class TestHTML(unittest.TestCase):
         with open("tests/resources/test.html", "r") as f:
             self.html = HTML(f.read())
 
-    def test_basic_html(self):
-        print self.html
+    def test_find_element_by_id(self):
+        self.assertEqual("nav-main mega-menu menu_float_left", self.html.find_element_by_id('primary-nav').get("class"))
 
-    def test_find_by_id(self):
-        print self.html
+    def test_find_by_elements_id(self):
+        self.assertEqual(2, len(self.html.find_elements_by_id('primary-nav')))
 
-    def test_find_by_xpath(self):
-        for item in self.html.tree.findall(".//{http://www.w3.org/1999/xhtml}a"):
-            print item.text
+    def test_find_element_by_xpath(self):
+        item = self.html.find_element_by_xpath(".//li[@class='dropdown-submenu test']/a[@href='index.html#']")
+        self.assertEqual("Press Room", item.text)
+
+    def test_find_elements_by_xpath(self):
+        items = self.html.find_elements_by_xpath(".//li[@class='dropdown-submenu test']/a[@href='index.html#']")
+        self.assertEqual(1, len(items))
