e732eb80565273b7583a9cb66ee55c925c8df2ec
ivan.martin <ivan.martin@11paths.com>
2016-12-15 14:32:22 +0100

    add fields to clear context

---
+++ tests/test_http_context.py
@@ -0,0 +1,46 @@
+import unittest
+
+from sdklib.http import HttpRequestContextSingleton, HttpRequestContext
+
+
+class TestHttpContext(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_http_context_sigleton(self):
+        ctxt_singleton = HttpRequestContextSingleton.get_instance()
+        ctxt_singleton.method = "POST"
+
+        ctxt_singleton2 = HttpRequestContextSingleton.get_instance()
+        self.assertEqual(ctxt_singleton.method, ctxt_singleton2.method)
+
+    def test_http_context_clear(self):
+        ctxt_singleton = HttpRequestContextSingleton.get_instance()
+        ctxt_singleton.method = "POST"
+        self.assertEqual("POST", ctxt_singleton.method)
+        ctxt_singleton.clear()
+        self.assertNotEqual("POST", ctxt_singleton.method)
+
+        ctxt_singleton2 = HttpRequestContextSingleton.get_instance()
+        self.assertNotEqual("POST", ctxt_singleton2.method)
+
+    def test_http_context_fields_to_clear(self):
+        ctxt_singleton = HttpRequestContextSingleton.get_instance()
+        ctxt_singleton.fields_to_clear = ['proxy']
+
+        ctxt_singleton.proxy = "http://localhost:8080"
+        ctxt_singleton.method = "PUT"
+        self.assertEqual("http://localhost:8080", ctxt_singleton.proxy)
+        ctxt_singleton.clear()
+        self.assertNotEqual("http://localhost:8080", ctxt_singleton.proxy)
+        self.assertEqual("PUT", ctxt_singleton.method)
+
+        ctxt_singleton2 = HttpRequestContextSingleton.get_instance()
+        self.assertNotEqual("http://localhost:8080", ctxt_singleton2.proxy)
+        self.assertEqual("PUT", ctxt_singleton2.method)
