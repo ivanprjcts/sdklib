1da2c32990552d0239db361604e52bd053249a38
Rubén González Alonso <rgonalo@gmail.com>
2016-12-16 11:22:21 +0100

    Use an internal logger instance to print request and response logs (#42)

--- sdklib/util/logger.py
+++ sdklib/util/logger.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import json
+import logging
 from xml.dom.minidom import parseString
 
 from sdklib.http.headers import CONTENT_TYPE_HEADER_NAME
 from sdklib.http.renderers import JSONRenderer, XMLRenderer
+
+logger = logging.getLogger(__name__)
 
 
 def _get_pretty_body(headers, body):
@@ -30,10 +33,9 @@ def _get_pretty_body(headers, body):
         return body
 
 
-def log_print_request(logger, method, url, query_params=None, headers=None, body=None):
+def log_print_request(method, url, query_params=None, headers=None, body=None):
     """
     Log an HTTP request data in a user-friendly representation.
-    :param logger: Logger to use
     :param method: HTTP method
     :param url: URL
     :param query_params: Query parameters in the URL
@@ -42,37 +44,34 @@ def log_print_request(logger, method, url, query_params=None, headers=None, body
     :return: None
     """
 
-    if logger:
-        log_msg = '\n>>>>>>>>>>>>>>>>>>>>> Request >>>>>>>>>>>>>>>>>>> \n'
-        log_msg += '\t> Method: %s\n' % method
-        log_msg += '\t> Url: %s\n' % url
-        if query_params is not None:
-            log_msg += '\t> Query params: {}\n'.format(str(query_params))
-        if headers is not None:
-            log_msg += '\t> Headers:\n{}\n'.format(json.dumps(dict(headers), sort_keys=True, indent=4))
-        if body is not None:
-            log_msg += '\t> Payload sent:\n{}\n'.format(_get_pretty_body(headers, body))
+    log_msg = '\n>>>>>>>>>>>>>>>>>>>>> Request >>>>>>>>>>>>>>>>>>> \n'
+    log_msg += '\t> Method: %s\n' % method
+    log_msg += '\t> Url: %s\n' % url
+    if query_params is not None:
+        log_msg += '\t> Query params: {}\n'.format(str(query_params))
+    if headers is not None:
+        log_msg += '\t> Headers:\n{}\n'.format(json.dumps(dict(headers), sort_keys=True, indent=4))
+    if body is not None:
+        log_msg += '\t> Payload sent:\n{}\n'.format(_get_pretty_body(headers, body))
 
-        logger.debug(log_msg)
+    logger.debug(log_msg)
 
 
-def log_print_response(logger, status_code, response, headers=None):
+def log_print_response(status_code, response, headers=None):
     """
     Log an HTTP response data in a user-friendly representation.
-    :param logger: __logger__ to use
     :param status_code: HTTP Status Code
     :param response: Raw response content (string)
     :param headers: Headers in the response (dict)
     :return: None
     """
 
-    if logger:
-        log_msg = '\n<<<<<<<<<<<<<<<<<<<<<< Response <<<<<<<<<<<<<<<<<<\n'
-        log_msg += '\t< Response code: {}\n'.format(str(status_code))
-        if headers is not None:
-            log_msg += '\t< Headers:\n{}\n'.format(json.dumps(dict(headers), sort_keys=True, indent=4))
-        try:
-            log_msg += '\t< Payload received:\n{}'.format(_get_pretty_body(headers, response))
-        except:
-            log_msg += '\t< Payload received:\n{}'.format(response)
-        logger.debug(log_msg)
+    log_msg = '\n<<<<<<<<<<<<<<<<<<<<<< Response <<<<<<<<<<<<<<<<<<\n'
+    log_msg += '\t< Response code: {}\n'.format(str(status_code))
+    if headers is not None:
+        log_msg += '\t< Headers:\n{}\n'.format(json.dumps(dict(headers), sort_keys=True, indent=4))
+    try:
+        log_msg += '\t< Payload received:\n{}'.format(_get_pretty_body(headers, response))
+    except:
+        log_msg += '\t< Payload received:\n{}'.format(response)
+    logger.debug(log_msg)
