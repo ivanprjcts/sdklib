b42c467d6c0976a03ebcfe67ce979531072156f8
Ivan Martin <ivan.martin@11paths.com>
2016-12-21 13:11:14 +0100

    add generate_url and redo urlsplit functions

--- sdklib/compat.py
+++ sdklib/compat.py
@@ -12,13 +12,15 @@ is_py3 = (_ver[0] == 3)
 if is_py2:
     import Cookie as cookies
     from urllib import urlencode, quote_plus
+    from urlparse import urlsplit
     import SocketServer as socketserver
     import thread
 
     basestring = basestring
     str = unicode
 elif is_py3:
-    from urllib.parse import urlencode, quote_plus
+    from urllib.parse import urlencode, quote_plus, urlsplit
+    from urllib.parse import urlencode
     from http import cookies
     import socketserver
     import _thread as thread
--- sdklib/util/urls.py
+++ sdklib/util/urls.py
@@ -1,22 +1,43 @@
 import re
 
+from sdklib.compat import urlencode, str, urlsplit as py_urlsplit
+
 
 def urlsplit(url):
-    p = '((?P<scheme>.*)?.*://)?(?P<host>[^:/ ]+).?(?P<port>[0-9]*).*'
+    """
+    Split url into scheme, host, port, path, query
 
+    :param str url:
+    :return: scheme, host, port, path, query
+    """
+    p = '((?P<scheme>.*)?.*://)?(?P<host>[^:/ ]+).?(?P<port>[0-9]*).*'
     m = re.search(p, url)
     scheme = m.group('scheme')
     host = m.group('host')
     port = m.group('port')
 
-    return scheme, host, port
+    _scheme, _netloc, path, query, _fragment = tuple(py_urlsplit(url))
+
+    return scheme, host, port, path, query
+
+
+def _get_scheme_or_default(scheme):
+    return scheme or 'http'
+
+
+def _get_host_or_default(host):
+    return host or '127.0.0.1'
+
+
+def _get_port_or_default(port, scheme):
+    return port or ('443' if scheme == 'https' else '80')
 
 
 def get_hostname_parameters_from_url(url):
-    scheme, host, port = urlsplit(url)
-    scheme = scheme or 'http'
-    host = host or '127.0.0.1'
-    port = port or ('443' if scheme == 'https' else '80')
+    scheme, host, port, _, _ = urlsplit(url)
+    scheme = _get_scheme_or_default(scheme)
+    host = _get_host_or_default(host)
+    port = _get_port_or_default(port, scheme=scheme)
     return scheme, host, port
 
 
@@ -34,3 +55,28 @@ def ensure_url_path_format_suffix_starts_with_dot(format_suffix):
     if not format_suffix.startswith('.'):
         return '.' + format_suffix
     return format_suffix
+
+
+def generate_url(scheme=None, host=None, port=None, path=None, query=None):
+    """
+    Generate URI from parameters.
+
+    :param str scheme:
+    :param str host:
+    :param int port:
+    :param str path:
+    :param dict query:
+    :return:
+    """
+    url = ""
+    if scheme is not None:
+        url += "%s://" % scheme
+    if host is not None:
+        url += host
+    if port is not None:
+        url += ":%s" % str(port)
+    if path is not None:
+        url += ensure_url_path_starts_with_slash(path)
+    if query is not None:
+        url += "?%s" % (urlencode(query))
+    return url
