7c57014e49a8f49caf9274615c1b1b67eb9a8f6d
Ivan Martin <ivanprjcts@gmail.com>
2017-01-12 17:25:08 +0100

    add some tests

--- tests/test_times.py
+++ tests/test_times.py
@@ -1,7 +1,27 @@
 import unittest
 import time
+from datetime import timedelta, datetime, tzinfo
 
-from sdklib.util.times import timeout
+from sdklib.util.times import timeout, datetime_to_milliseconds_timestamp, seconds_to_milliseconds_timestamp
+
+
+class GMT1(tzinfo):
+    def utcoffset(self, dt):
+        return timedelta(hours=1) + self.dst(dt)
+
+    def dst(self, dt):
+        # DST starts last Sunday in March
+        d = datetime(dt.year, 4, 1)   # ends last Sunday in October
+        self.dston = d - timedelta(days=d.weekday() + 1)
+        d = datetime(dt.year, 11, 1)
+        self.dstoff = d - timedelta(days=d.weekday() + 1)
+        if self.dston <=  dt.replace(tzinfo=None) < self.dstoff:
+            return timedelta(hours=1)
+        else:
+            return timedelta(0)
+
+        def tzname(self,dt):
+            return "GMT +1"
 
 
 class TestTimes(unittest.TestCase):
@@ -23,3 +43,7 @@ class TestTimes(unittest.TestCase):
 
         res = my_long_function()
         self.assertIsNone(res)
+
+    def test_seconds_to_milliseconds_timestamp(self):
+        res = seconds_to_milliseconds_timestamp(1015801200000)
+        self.assertEqual(res, 1015801200000000)
