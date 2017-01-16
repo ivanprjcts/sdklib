10ba5554eee5e203158b1d18b8cd98e6c13a6a5a
Ivan Martin <ivanprjcts@gmail.com>
2017-01-16 18:11:02 +0100

    add some tests

--- sdklib/util/files.py
+++ sdklib/util/files.py
@@ -7,9 +7,3 @@ def guess_filename_stream(path_to_file):
     f.close()
     filename = ntpath.basename(path_to_file)
     return filename, buf
-
-
-def read_file_as_string(file_path):
-    with open(file_path, 'rb') as myfile:
-        data = myfile.read()
-    return data
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,9 @@
 import unittest
 
-from sdklib.util.parser import parse_args_as_tuple_list, parse_params_as_tuple_list
+from sdklib.util.parser import (
+    parse_args_as_tuple_list, parse_params_as_tuple_list, safe_add_slash, safe_add_end_slash, get_url_query_params,
+    parse_params
+)
 
 
 class TestParser(unittest.TestCase):
@@ -10,7 +13,33 @@ class TestParser(unittest.TestCase):
         self.assertIn(("param", "value"), res)
         self.assertIn(("number", 1), res)
 
+    def test_parse_args_as_tuple_list_with_array(self):
+        res = parse_args_as_tuple_list(param="value", number=1, array=[1, 2, 3])
+        self.assertIn(("param", "value"), res)
+        self.assertIn(("number", 1), res)
+        self.assertIn(("array", 1), res)
+
     def test_parse_params_as_tuple_list(self):
         res = parse_params_as_tuple_list({"param": "value", "number": 1})
         self.assertIn(("param", "value"), res)
         self.assertIn(("number", 1), res)
+
+    def test_safe_add_slash(self):
+        res = safe_add_slash("path/to/something")
+        self.assertEqual("/path/to/something", res)
+
+    def test_safe_add_slash_blank(self):
+        res = safe_add_slash(None)
+        self.assertEqual("", res)
+
+    def test_safe_add_end_slash(self):
+        res = safe_add_end_slash(None)
+        self.assertEqual("", res)
+
+    def test_get_url_query_params(self):
+        res = get_url_query_params({"param": "value"})
+        self.assertEqual("?param=value", res)
+
+    def test_parse_params(self):
+        res = parse_params({"param": "value", "param2": None})
+        self.assertEqual({"param": "value"}, res)
